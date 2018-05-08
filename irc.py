d3dd6c8784163ed0a8f6885ad08a37afaf94d8ac
Molly Wood <mollywood@Interzone.localdomain>
2018-05-08 10:42:57 -0500

    waiting for MOTD

--- irc.py
+++ irc.py
@@ -9,20 +9,21 @@ class IRC:
         self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def send(self, chan, msg):
-        self.irc.send("PRIVMSG " + chan + " " + msg + "n")
+        self.irc.send("PRIVMSG " + chan + " " + msg + "\n")
 
     def connect(self, server, channel, botnick):
         #defines the socket
         print("connecting to:"+server)
         self.irc.connect((server, 6667))
-        self.irc.send(bytes("USER " + botnick + " " + botnick + " " + botnick + " :This is a fun bot!n", "UTF-8"))
-        self.irc.send(bytes("NICK " + botnick + "n", "UTF-8"))
-        self.irc.send(bytes("JOIN " + channel + "n", "UTF-8")) #join the channel
+        self.irc.send(bytes("USER " + botnick + " " + botnick + " " + botnick + " :This is a fun bot!\n", "UTF-8"))
+        self.irc.send(bytes("NICK " + botnick + "\n", "UTF-8"))
+        if self.irc.recv(376):
+            self.irc.send(bytes("JOIN " + channel + "\n", "UTF-8")) #join the channel
 
     def get_text(self):
         text = self.irc.recv(2040) #receive the get_text
 
-        if text.find(bytes('PING', 'UTF-8')) != -1:
-            self.irc.send('PONG ' + text.split() [1] + 'rn')
+        if text.find(bytes('PING', "UTF-8")) != -1:
+            self.irc.send(bytes('PONG ', "UTF-8" + text.split() [1] + bytes('\r\n', "UTF-8"))
 
         return text
